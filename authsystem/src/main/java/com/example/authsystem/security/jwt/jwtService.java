ed8b28042eb311a38a2e94192567da54d50a69d7
ahmed elbaz <ahmedelbaz771@gmail.com>
2024-02-25 11:20:29 +0100

    Create JwtService class to handle JWT token generation, validation, and username extraction

---
+++ authsystem/src/main/java/com/example/authsystem/security/jwt/jwtService.java
@@ -0,0 +1,58 @@
+package com.example.authsystem.security.jwt;
+
+import com.example.authsystem.appuser.AppUser;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class jwtService {
+
+    private final String SECRET_KEY="27e4a92f2381e6758a60234cfc04eb30471aa84d44640e4c7e91d53d1f0ef0e1";
+
+    public boolean isValid(String token, AppUser appUser){
+        String username = extractUsername(token);
+        return username.equals(appUser.getUsername());
+    }
+
+    public String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> resolver){
+        Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parser()
+                .verifyWith(getSigninKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String generateToken(AppUser appUser){
+        String token = Jwts
+                .builder()
+                .subject(appUser.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 24*60*60*1000))
+                .signWith(getSigninKey())
+                .compact();
+
+        return token;
+    }
+
+    public SecretKey getSigninKey(){
+        byte[] keyBytes = Decoders.BASE64URL.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
